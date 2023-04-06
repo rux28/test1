1636383f400e6d1d3a9ea9ec0c861aae7228b81e
rux <r.m.horhat@gmail.com>
2023-04-06 19:01:02 +0300

    clasa

---
+++ cmake-build-debug/Ex2derivata.h
@@ -0,0 +1,14 @@
+//
+// Created by Tibi on 4/6/2023.
+//
+
+#ifndef TEST1_EX2DERIVATA_H
+#define TEST1_EX2DERIVATA_H
+
+
+class Ex2derivata{
+
+};
+
+
+#endif //TEST1_EX2DERIVATA_H
---
+++ ex2baza.cpp
@@ -0,0 +1,10 @@
+//
+// Created by Tibi on 4/6/2023.
+//
+
+#include "ex2baza.h"
+//int a;
+//int afis()
+//{
+//    return a;
+//}
